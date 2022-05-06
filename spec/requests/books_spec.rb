4d38cde51139414fd4dbff5d23d792f1e18ae009
Samuel Cornet <corsam28@gmail.com>
2022-05-05 23:33:02 -0400

    build test for deleting books

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -16,10 +16,22 @@ describe 'Books API' do
   context 'POST /books' do
     it 'create a new book' do
       expect {  
-        post '/api/v1/books', params: {book: { title: 'Game od Thrones', author: 'Martin Blue'}}
+        post '/api/v1/books', params: {book: { title: 'Game of Thrones', author: 'Martin Blue'}}
       }.to change { Book.count }.from(0).to(1)
 
        expect(response).to have_http_status(:created)
     end 
+  end
+
+  describe 'DELETE /books:id' do
+    let!(:book) { FactoryBot.create(:book, title: 'The Little Tiny Boy', author: 'George Neil') }
+      it 'delete a book' do
+        expect {  
+          delete "/api/v1/books/#{book.id}"
+        }.to change { Book.count }.from(1).to(0)
+
+      expect(response).to have_http_status(:no_content)
+    end 
+      
   end
 end
