2cb34983fde1ce7c1ea372b100b6f76b935915be
Samuel Cornet <corsam28@gmail.com>
2022-05-05 23:00:07 -0400

    build test for adding books

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
 describe 'Books API' do
-  it 'returns all books' do
-    FactoryBot.create(:book, title: 'The Little Tiny Boy', author: 'George Neil')
-    FactoryBot.create(:book, title: 'The Tom Boy', author: 'Danielle Style')
+  context 'GET /books' do
+    it 'returns all books' do
+      FactoryBot.create(:book, title: 'The Little Tiny Boy', author: 'George Neil')
+      FactoryBot.create(:book, title: 'The Tom Boy', author: 'Danielle Style')
 
-    get '/api/v1/books'
+      get '/api/v1/books'
 
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(2)  
-  end    
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
+
+  context 'POST /books' do
+    it 'create a new book' do
+      expect {  
+        post '/api/v1/books', params: {book: { title: 'Game od Thrones', author: 'Martin Blue'}}
+      }.to change { Book.count }.from(0).to(1)
+
+       expect(response).to have_http_status(:created)
+    end 
+  end
 end
