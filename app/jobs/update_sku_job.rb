d7ca11f728033394ec7fa5c7b1cf521320d825af
Samuel Cornet <corsam28@gmail.com>
2022-06-09 18:42:21 -0400

    Test Active Job with RSpec

--- app/jobs/update_sku_job.rb
+++ app/jobs/update_sku_job.rb
@@ -1,10 +1,13 @@
+
+require 'net/http'
+
 class UpdateSkuJob < ApplicationJob
   queue_as :default
 
   def perform(book_name)
     uri = URI('http://localhost:3000/update_sku')
-    req = Net::HTTP::POST.new(uri, 'Content-Type =>application/json')
-    req.body = { sku: '123', name: book_name[:name] }.to_json
+    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+    req.body = { sku: '123', name: book_name }.to_json
     res = Net::HTTP.start(uri.hostname, uri.port) do |http|
       http.request(req)
     end
--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,9 +1,25 @@
 require "rails_helper"
 
-RSspec.describe Api::V1::BooksController, type: :controller do
+RSpec.describe Api::V1::BooksController, type: :controller do
+  describe 'GET index' do
     it 'has a max limit of 100' do
       expect(Book).to receive(:limit).with(100).and_call_original
 
       get :index, params: { limit: 999 }
-    end    
+    end 
+  end
+
+  describe 'POST create' do
+    let(:book_name) { "Harry Potter" }
+
+    it 'calls UpdateSkuJob with correct params' do
+      expect(UpdateSkuJob).to receive(:perform_later).with(book_name)
+
+
+      post :create, params: { 
+        book: {title: book_name},
+        author: {first_name: "Dan", last_name: "Wallas", age: 67} 
+      }
+    end 
+  end
 end
